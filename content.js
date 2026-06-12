f6c40b4f8ff9ce536b3c01b60d0421af1a984502
Oscar Tu <oscartu20021217@gmail.com>
2026-06-12 17:39:51 -0400

    Previous/Next/Play/Pause button complete

--- content.js
+++ content.js
@@ -6,23 +6,35 @@ browser.runtime.onMessage.addListener(async (message) => {
     if (message.action === "getSong") {
         var songTitle = document.querySelector(".title.style-scope.ytmusic-player-bar").textContent;
         return {
-            message: songTitle
+            message: songTitle,
         };
     }
 
     // play, pause music
     if (message.action === "play") {
-
+        var playButton = document.querySelector(".play-pause-button.style-scope.ytmusic-player-bar").querySelector(".style-scope.yt-icon-button");
+        playButton.click();
+        return {
+            action: "clicked"
+        };
     }
 
     // next song
     if (message.action === "nextSong") {
-
+        var nextButton = document.querySelector(".next-button.style-scope.ytmusic-player-bar").querySelector(".style-scope.yt-icon-button");
+        nextButton.click();
+        return {
+            action: "clicked"
+        };
     }
 
     // previous song
     if (message.action === "prevSong") {
-
+        var prevButton = document.querySelector(".previous-button.style-scope.ytmusic-player-bar").querySelector(".style-scope.yt-icon-button");
+        prevButton.click();
+        return {
+            action: "clicked"
+        };
     }
 });
 
--- src/menu.js
+++ src/menu.js
@@ -30,15 +30,59 @@ browser.runtime.onMessage.addListener((message) => {
     }
 });
 
+document.getElementById("play").addEventListener("click", playPause);
+async function playPause() {
+    const tabs = await browser.tabs.query({
+        url: "*://music.youtube.com/*"
+    });
 
-function prevSong() {
+    // is not opened
+    if (tabs.length === 0) {
+        return;
+    }
 
+    const response = await browser.tabs.sendMessage(
+        tabs[0].id,
+        { action: "play" }
+    );
+
+    console.log("Play-Pause: ", response.action);
 }
 
-function nextSong() {
 
+document.getElementById("next").addEventListener("click", nextSong);
+async function nextSong() {
+    const tabs = await browser.tabs.query({
+        url: "*://music.youtube.com/*"
+    });
+
+    // is not opened
+    if (tabs.length === 0) {
+        return;
+    }
+
+    const response = await browser.tabs.sendMessage(
+        tabs[0].id,
+        { action: "nextSong" }
+    );
+    console.log("Next Song: ", response.action);
 }
 
-function playPause() {
+document.getElementById("prev").addEventListener("click", prevSong);
+async function prevSong() {
+    const tabs = await browser.tabs.query({
+        url: "*://music.youtube.com/*"
+    });
 
+    // is not opened
+    if (tabs.length === 0) {
+        return;
+    }
+
+    const response = await browser.tabs.sendMessage(
+        tabs[0].id,
+        { action: "prevSong" }
+    );
+
+    console.log("Previous Song: ", response.action);
 }
